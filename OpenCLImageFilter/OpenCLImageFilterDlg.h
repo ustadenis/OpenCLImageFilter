00f32726f98ae61cef85d9bb8d34f58940035817
AlexUgol <alex@nifti.unn.ru>
2016-04-28 16:05:28 +0300

    Добавлена конфигурация для VS 2015. Коррекция поведения интерфейса при выборе платформы и устройства. Обработка ошибки сборки программы OCL.

--- OpenCLImageFilter/OpenCLImageFilterDlg.h
+++ OpenCLImageFilter/OpenCLImageFilterDlg.h
@@ -66,7 +66,7 @@ public:
 	afx_msg void OnCbnSelchangePlatformscombo();
 	afx_msg void OnBnClickedGetdevicesbutton();
 	afx_msg void OnCbnSelchangeDevicescombo();
-	afx_msg void OnBnClickedInitbutton();
+//	afx_msg void OnBnClickedInitbutton();
 	afx_msg void OnBnClickedAddnoisebutton();
 	afx_msg void OnBnClickedStartbutton();
 	afx_msg void OnBnClickedBrowsebutton();
